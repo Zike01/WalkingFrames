0ccb8592792f52397a9496ee39a0274ba0667524
Zike_01 <zikekomakoma@gmail.com>
2023-08-19 11:57:54 +0300

    Remove datetime import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import json
-from datetime import datetime
 from bot import TwitterBot
 from dotenv import load_dotenv
 
@@ -28,44 +27,44 @@ curr_season = data["curr_season"]
 curr_ep = data["curr_ep"]
 curr_frame = data["curr_frame"]
 
-current_time = datetime.now()
+image_path = f"images\\S{curr_season:02d}\\{curr_ep:02d}\\frame_{curr_frame}.jpg"
 
-if current_time.minute % 15 == 0:
-    image_path = f"images\\S{curr_season:02d}\\{curr_ep:02d}\\frame_{curr_frame}.jpg"
-    # Upload media file if image exists
+# Upload media file if image exists
+try:
+    media = clientv1.media_upload(image_path)
+except FileNotFoundError:
+    # Go to the first frame of the next episode if image path does not exist
+    curr_ep += 1
+    curr_frame = 1
     try:
         media = clientv1.media_upload(image_path)
     except FileNotFoundError:
-        # Go to the first frame of the next episode if image path does not exist
-        curr_ep += 1
+        # Go to the first ep of the next season if there is no next episode
+        curr_season += 1
+        curr_ep = 1
         curr_frame = 1
         try:
             media = clientv1.media_upload(image_path)
         except FileNotFoundError:
-            # Go to the first ep of the next season if there is no next episode
-            curr_season += 1
-            curr_ep = 1
-            curr_frame = 1
-            try:
-                media = clientv1.media_upload(image_path)
-            except FileNotFoundError:
-                sys.exit()
+            sys.exit()
 
-    # Get media id of the newly uploaded image
-    media_id = media.media_id
+# Get media id of the newly uploaded image
+media_id = media.media_id
 
-    # Get the total number of frames in the episodes
-    current_dir = f"images\\S{curr_season:02d}\\{curr_ep:02d}"
-    total_frames = len(os.listdir(current_dir))
+# Get the total number of frames in the episodes
+current_dir = f"images\\S{curr_season:02d}\\{curr_ep:02d}"
+total_frames = len(os.listdir(current_dir))
 
-    text = f"The Walking Dead - Season {curr_season:02d} Episode {curr_ep:02d} - Frame {curr_frame} of {total_frames}"
 
-    # upload the tweet
-    response = clientv2.create_tweet(text=text, media_ids=[media_id])
-    print(text)
-    print(f"https://twitter.com/walking_frames/status/{response.data['id']}")
+# upload the tweet
+text = f"The Walking Dead - Season {curr_season:02d} Episode {curr_ep:02d} - Frame {curr_frame} of {total_frames}"
+response = clientv2.create_tweet(text=text, media_ids=[media_id])
 
-    curr_frame += 1
+# print link to the tweet
+print(text)
+print(f"https://twitter.com/walking_frames/status/{response.data['id']}")
+
+curr_frame += 1
 
 # Store newly updated values to the config file
 data["curr_season"] = curr_season
