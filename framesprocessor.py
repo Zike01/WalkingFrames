8423f74da0e699b60914b0d5bce93d6b50baefba
Zike_01 <zikekomakoma@gmail.com>
2023-09-21 20:05:43 +0100

    Fix file to process any number of episodes

--- framesprocessor.py
+++ framesprocessor.py
@@ -5,43 +5,42 @@ import os
 # -----------------CONSTANTS-------------------#
 VIDEO_PATH = ""
 IMAGE_PATH = ""
-EXTENSION = ""
 TARGET_FPS = 1
 # ---------------------------------------------#
 
 
-def process(season, episode):
-    video = f"{VIDEO_PATH}/S{season}/S{season}e{episode}.{EXTENSION}"
+def process(file):
+    video = f"{VIDEO_PATH}/{file}"
     capture = cv2.VideoCapture(video)
     fps = round(capture.get(cv2.CAP_PROP_FPS))
 
-    skip = round(fps / TARGET_FPS)
     curr_frame = 0
-    frame_made = 0
+    frame_count = 0
     while True:
         success, frame = capture.read()
 
         if not success:
-            return
+            break
 
         # Only save the image if the current frame is divisible by the fps of the original video
         # This way only 1 frame is processed per second
 
-        if curr_frame % skip == 0:
-            frame_made += 1
-            exists = os.path.isdir(f"{IMAGE_PATH}/S{season}/{episode}")
+        if curr_frame % fps == 0:
+            frame_count += 1
+            episode = int(file.split(".")[0].split("e")[1])
+            exists = os.path.isdir(f"{IMAGE_PATH}/S{season:02d}/{episode:02d}")
 
             if not exists:
-                os.mkdir(f"{IMAGE_PATH}/S{season}/{episode}")
+                os.makedirs(f"{IMAGE_PATH}/S{season:02d}/{episode:02d}")
 
-            name = f"{IMAGE_PATH}/S{season}/{episode}/frame_{frame_made}.jpg"
+            name = f"{IMAGE_PATH}/S{season:02d}/{episode:02d}/frame_{frame_count}.jpg"
             cv2.imwrite(name, frame)
         curr_frame += 1
+    capture.release()
 
 
 if __name__ == "__main__":
     season = int(input("Season: "))
-    eps = len(os.listdir(f"{VIDEO_PATH}/S{season:02d}"))
-
-    for ep in range(1, eps + 1):
-        process(f"{season:02d}", f"{ep:02d}")
+    eps = os.listdir(f"{VIDEO_PATH}/S{season:02d}")
+    for ep in eps:
+        process(ep)
